6860706df2f7650b82d45e09346f8a1aee54d83e
Hassaan Maqsood <50172619+hassaanmaqsood@users.noreply.github.com>
2022-12-21 09:05:59 +0000

    git commit dbOps.js

---
+++ dbOps.js
@@ -0,0 +1,10 @@
+// use table.js to create database functions
+
+/* 
+Basic Ops
+    -Create Database    
+    -Perform Querries with Logics AND and OR using single function
+    -Add Entries
+Advanced Ops
+    -Predict the missing Value/Entry
+*/
